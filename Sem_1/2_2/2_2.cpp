608738a89268c090ae305e714cc926012e669d6f
killpureskill <153020799+killpureskill@users.noreply.github.com>
2024-01-09 21:57:07 +0500

    Add files via upload

---
+++ Sem_1/2_2/2_2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    int n;
+    int sum = 1;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a;
+        suma *= a;
+    }
+    cout << sum;
+}
