0d8402d342a742864500a93c03673e64652aef10
killpureskill <153020799+killpureskill@users.noreply.github.com>
2024-01-11 05:52:34 +0500

    Add files via upload

---
+++ Sem_1/2_15/2_15.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, a, b = 1;
+    cin >> n >> a;
+    while (true) {
+        b = n % 10;
+        if (a == b) {
+            cout << "true" << endl;
+        }
+        if (n < 10) {
+            return 1;
+        }
+        n = n / 10;
+    }
+}
