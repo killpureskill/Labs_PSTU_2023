60b8605b76bd75b380a6fdad91c761aa7f627043
killpureskill <153020799+killpureskill@users.noreply.github.com>
2024-01-11 05:28:56 +0500

    Add files via upload

---
+++ Sem_1/2_12/2_12.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int n, x, max = -10000, min = 10000;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> x;
+		if (x >= max_0) {
+			max = x;
+		}
+		if (x <= min) {
+			min = x;
+		}
+	}
+	cout << max + min << endl;
+}
