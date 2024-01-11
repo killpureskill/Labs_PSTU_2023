263480e593d83872397769f435cb07926d40a284
killpureskill <153020799+killpureskill@users.noreply.github.com>
2024-01-11 07:09:25 +0500

    Add files via upload

---
+++ Sem_1/2_23/2_23.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    float n, a, max;
+    int i = 2;
+    int count = 1;
+    cin >> n;
+
+    max = sin(n + 1 / n);
+
+    while (i <= n)
+    {
+        a = sin(n + i / n);
+        if (a > max)
+        {
+            max = a;
+            count = 1;
+        }
+        else if (a == max)
+        {
+            count++;
+        }
+        i++;
+    }
+    cout << "max " << max;
+    cout << "count " << count;
+}
